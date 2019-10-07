51a8671ca0a45e415f4fefd6623956256c8b28dd
oztugrul <45922919+oztugrul@users.noreply.github.com>
2019-10-08 00:25:43 +0300

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -19,13 +19,13 @@ with(data_subset,{
   
   plot(Voltage~Date_Time,type="l",ylab="Voltage (Volt)",xlab="datetime")
   
-  plot(Sub_metering_1~Date_Time,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+  plot(Sub_metering_1~Date_Time,type="l",ylab="Energy sub metering",xlab="",ylim=c(0,40))
   
   lines(Sub_metering_2~Date_Time,col="red")
   
   lines(Sub_metering_3~Date_Time,col="blue")
   
-  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=2,bty = "n")
+  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=2,bty = "n",cex=0.9)
   
   plot(Global_reactive_power~Date_Time,type="l",ylab="Global Reactive Power (kilowatts)",xlab="datetime",ylim=c(0,0.5))
 })
